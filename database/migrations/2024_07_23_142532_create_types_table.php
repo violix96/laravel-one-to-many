d7ca07b1f712cb37d589c1d8ced9bf046d3a10fc
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-23 16:39:02 +0200

    [UPD] created the migration for the types table

--- database/migrations/2024_07_23_142532_create_types_table.php
+++ database/migrations/2024_07_23_142532_create_types_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 25)->unique();
+            $table->string('slug', 25);
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('types');
     }
 };
